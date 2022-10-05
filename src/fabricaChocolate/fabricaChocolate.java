fd4d8da978bef93bcf419194dc78430deb71da88
Thais <thaisrozas@gmail.com>
2022-10-05 11:02:13 -0300

    Primeiras alterações

---
+++ src/fabricaChocolate/fabricaChocolate.java
@@ -0,0 +1,49 @@
+package src.fabricaChocolate;
+
+public class fabricaChocolate {
+    private boolean empty; //vazio
+    private boolean boiled; //fervido
+
+    //C�digo iniciado quando a Caldeira est� vazia
+    public fabricaChocolate() {     //public constructor!
+        empty = true;
+        boiled = false;
+    }
+
+    //Para encher a caldeira, ela deve estar vazia e, quando estiver cheia,
+    //configuramos os indicadores de empty e boiled
+    public void preencher() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // encher a caldeira com uma mistura de leite/chocolate
+        }
+    }
+
+    //Para drenar a caldeira, ela deve estar cheia (n�o vazia) e tamb�m em ebuli��o.
+    //Depois, configuramos empty como true novamente.
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // drena na caldeira leite e chocolate
+            empty = true;
+        }
+    }
+
+
+    //Metodo para ferver
+    //Para ferver a mistura, a caldeira deve estar cheia e ainda n�o deve ter fervido.
+    //Depois de ferver, configuramos o indicador boiled como true.
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
